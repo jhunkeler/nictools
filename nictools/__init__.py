da04743ccc602a91810a5cbcea0c637861f1e6d3
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-27 22:41:09 -0400

    Implement RECON

--- nictools/__init__.py
+++ nictools/__init__.py
@@ -17,5 +17,5 @@ Utility and library functions used by these tasks are also included in this
 module.
 """
 from __future__ import absolute_import
-from .version import * 
+from .version import *
 
--- setup.py
+++ setup.py
@@ -1,16 +1,44 @@
 #!/usr/bin/env python
+import recon.release
+from setuptools import setup, find_packages
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+
+version = recon.release.get_info()
+recon.release.write_template(version, 'nictools')
 
 
 setup(
-    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3.2'],
-    d2to1=True,
-    use_2to3=False,
-    zip_safe=False
+    name = 'nictools',
+    version = version.pep386,
+    author = 'Vicki Laidler, David Grumm',
+    author_email = 'help@stsci.edu',
+    description = 'Python Tools for NICMOS Data',
+    url = 'https://github.com/spacetelescope/nictools',
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [
+        'astropy',
+        'nose',
+        'numpy',
+        'pytools',
+        'scipy',
+        'sphinx',
+        'stsci.sphinxext',
+        'stsci.tools',
+    ],
+    packages = find_packages(),
+    package_data = {
+        'nictools': [
+            'pars/*',
+            'tests/*.fits',
+            '*.rules',
+            'LICENSE.txt'
+        ]
+    },
 )
